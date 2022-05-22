46e6fe8d5d0eec7acac99faa0e89b4d857e84876
wachiousylla <97784966+wachiousylla@users.noreply.github.com>
2022-05-22 11:21:03 +0100

    test
    test

---
+++ apropos.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="One Page Corporate HTML Template">
+    <meta name="author" content="">
+    <link rel="icon" href="images/logo1.png">
+
+    <!-- Title -->
+    <title>A PROPOS - CREDIT DE PROXIMITE POUR TOUS</title>
+
+    <!-- Necessary CSS Files -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">                                                <!-- Bootstrap CSS Only GRID -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/google-font.css">                                                  <!-- Google Font -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">                                             <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="css/swiper.min.css">                                                   <!-- Swiper slider -->
+    <link rel="stylesheet" href="css/jquery.mb.YTPlayer.min.css">                                       <!-- Video background -->
+    <link rel="stylesheet" href="css/template.css">                                                     <!-- Template CSS -->
+<style type="text/css">
+.goog-te-banner-frame{
+            display: none!important;
+        }
+        body{
+            position: static!important;
+            min-height: initial!important;
+        }
+.goog-tooltip{ display:  none!important;}
+.goog-text-highlight {
+  box-shadow: none !important;
+  background-color: transparent !important;
+  }
+    footer {border-top: 5px solid #c52d2f}
+    .colorier{color: #c52d2f}
+    .centerpp{
+      text-align: center;
+    }
+.marg{margin-bottom: 100px;}
+
+    .titr{
+      font-size: 20px;
+      font-weight: bold;
+    }
+
+.formmarg {
+    margin-top: 45px;
+}
+    </style>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<!-- Wrapper -->
+<div class="site-wrapper">
+
+    <!-- Top Bar -->
+    <div class="site-top-bar">
+
+        <!-- Bootstrap -->
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+
+                    <!-- Left section -->
+                    <div class="site-top-bar-left-section">
+
+                        <!-- Link -->
+                        <a>
+                            <!-- Font awesome icon -->
+                            <i class="fa fa-clock-o"></i>
+                            <!-- Text -->
+                            <span>Du Lundi au Vendredi : 08:00 - 18:00 & Samedi 09:00 - 14:00</span>
+                        </a>
+
+                    </div>
+                    <div class="site-top-bar-right-section">
+
+                        <!-- Link -->
+                        <a href="javascript:void()"  onclick="window.location.hash='#googtrans(en)';location.reload();"><img src="images/en.png" align="center"  alt="crédit en Angleterre" /> </a>&nbsp;
+                        <a href="javascript:void()"  onclick="window.location.hash='#googtrans(de)';location.reload();"><img src="images/de.png" align="center"  alt="crédit en Allemagne" /> </a>&nbsp;
+                        <a href="javascript:void()"  onclick="window.location.hash='#googtrans(es)';location.reload();"><img src="images/es.png" align="center"  alt="crédit en Espagne" />  </a>&nbsp;
+                        <a href="javascript:void()"  onclick="window.location.hash='#googtrans(fr)';location.reload();"><img src="images/fr.png" align="center"  alt="crédit en France" />  </a>&nbsp;
+                        <a href="javascript:void()"  onclick="window.location.hash='#googtrans(it)';location.reload();"><img src="images/ir.png" align="center"  alt="crédit en Italie" />  </a>&nbsp;
+                         <a href="javascript:void()"  onclick="window.location.hash='#googtrans(ro)';location.reload();"><img src="images/ro.png" align="center"  alt="crédit en Roumanie" /> </a>&nbsp;
+                          <a href="javascript:void()"  onclick="window.location.hash='#googtrans(mn)';location.reload();"><img src="images/mn.png" align="center"  alt="crédit en Mongolie" /> </a>&nbsp;
+                          <a href="javascript:void()"  onclick="window.location.hash='#googtrans(fi)';location.reload();"><img src="images/flag1.png" style="width: 25px" align="center"  alt="crédit en Mongolie" /> </a>&nbsp;
+                    </div>
+                    <!-- End Left Section -->
+
+
+                    <!-- Right Section -->
+                    <div class="site-top-bar-left-section">
+
+                        <!-- Link -->
+                        <a href="tel:+33755403488">
+                            <!-- Icon -->
+                            <i class="fa fa-phone"></i>
+                            <!-- Text -->
+                            <span>+33 755 403 488</span>
+                        </a>
+                        <a href="tel:+40771132810">
+                            <!-- Icon -->
+                            <i class="fa fa-phone"></i>
+                            <!-- Text -->
+                            <span>+40 771 132 810</span>
+                        </a>
+
+                        <!-- Link -->
+                        <a href="mailto:contact@credit-de-proximite-pour-tous.com">
+                            <!-- Icon -->
+                            <i class="fa fa-envelope"></i>
+                            <!-- Text -->
+                            <span>contact@credit-de-proximite-pour-tous.com</span>
+                        </a>
+
+                    </div>
+                    <!-- End Right Section -->
+
+                </div>
+            </div>
+        </div>
+        <!-- End Bootstrap -->
+
+    </div>
+    <!-- End Top Bar -->
+
+    <!-- Header -->
+    <header class="site-header" id="sticky-header">
+
+        <!-- Bootstrap -->
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+
+                    <!-- Logo -->
+                    <div class="site-logo">
+                        <!-- Link -->
+                        <a href="/">
+                            <!-- Logo Image -->
+                            <img src="images/logo1.png" style="width: 120px" alt="Logo">
+                        </a>
+                    </div>
+                    <!-- End logo -->
+
+                    <!-- Navigation Toggle Button -->
+                    <div class="site-nav-toggle">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <!-- End Nav Toggle Button -->
+
+                    <!-- Navigation -->
+                    <nav class="site-nav">
+                        <ul>
+                            <!-- Active Item (Use the active class) -->
+                            <li class=""><a href="/">Acceuil</a></li>
+                            <li class="active"><a href="apropos.php">A Propos</a></li>
+                            <li class=""><a href="services.php">services</a></li>
+                            <li class=""><a href="Procedure.php">Procédure</a></li>
+                            <li class=""><a href="Emprunter.php">Emprunter</a></li>
+                            <li class=""><a href="Conditions.php">Conditions</a></li>
+                            <li class=""><a href="Pret-CAF.php">Prêt CAF</a></li>
+                            <li class=""><a href="Pret-etudiant.php">Prêt étudiant</a></li>
+                            <li class=""><a href="Pret-sur-gage.php">Prêt sur gage<br>Crédit trésorier</a></li>
+                        </ul>
+                    </nav>
+                    <!-- End Navigation -->
+
+                </div>
+            </div>
+        </div>
+        <!-- End Bootstrap -->
+
+    </header>
+    <!-- End Header -->
+    
+    <!-- This section classes require for whole page sliders -->
+    <div id="sequence" style="display:none;">
+        <ul class="seq-canvas">
+        </ul>
+    </div>
+
+    <!-- End Slider -->
+
+    <!-- Main banner -->
+    <div class="inner-page-main-banner about-us">
+		<!-- Bootstrap -->
+		<div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+
+                    <!-- H2 heading -->
+                    <h1>A Propos de Nous</h1>
+                    <!-- Bredcum links -->
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-sm-offset-3">
+                        	<ul>
+                            	<li> <a href="index.html">Acceuil » </a> </li>
+                                <li>Apropos</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+		</div>
+        <!-- End bootstrap -->
+    </div>
+    <!-- End Main banner -->
+
+	<!-- Vision mission -->
+    <div id="vision-mission" class="page-vision-mission site-white-section">
+
+        <!-- Bootstrap -->
+        <div class="container">
+        	<!-- upper section -->
+        	<div class="upper-section">
+	            <div class="row">
+                <div class="col-xs-12">
+
+                    <!-- Clearfix -->
+                    <div class="clearfix"></div>
+
+                        <!-- Bootstrap nested columns -->
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-6">
+								<div class="left-box">
+	                                <!-- Figure -->
+    	                            <figure>
+        	                            <img src="images/groupe1.jpg" alt="image">
+            	                    </figure>
+								</div>
+                            </div>
+                            
+                            <div class="col-xs-12 col-sm-12 col-md-6">
+								<div class="right-box">
+                                    <!-- H3 heading -->
+                                    <h3>Notre VisSion et Mission</h3>
+
+                                    <!-- Paragraph -->
+                                    <p>
+                                        CREDIT DE PROXIMITE POUR TOUS comprend des experts de la propriété de crédit
+<a href="Emprunter.php">Cliquez ici pour toutes demandes de crédit</a> et mobilisées pour écouter et servir ses clients, Un réseau d'agents Unis.  CREDIT DE PROXIMITE POUR TOUS se rend disponible et vous apporte les meilleures conditions de financement pour un prêt d'argent entre particuliers france et aux entreprises.
+                                    </p>
+                                    
+								</div>
+
+                            </div>
+                        </div>
+
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- End Bootstrap -->
+
+    </div>
+    <!-- End Site Features  -->
+    
+    <!-- Overview -->
+    <div id="overview" class="overview-section inner-page-white-section">
+
+        <!-- Bootstrap -->
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+
+                    <!-- H2 heading -->
+                    <h2>INFORMATIONS GENERALES
+                    </h2>
+
+                    <!-- Left colom -->
+                    <div class="overview">
+                        <!-- Paragraph -->
+                        <p>Nous octroyons tous types de crédit à savoir: Rachat de credit que ça soit Rachat de crédit consommation ou Rachat de crédit immobilier Prêt renouvelable Prêt auto Prêt moto Prêt travaux Prêt étudiant Prêt CAF Prêt sur gage Crédit consommation Crédit auto Prêt moto Crédit vacances Crédit renouvelable Prêt personnel Prêt trésorerie particulier Crédit profession libérale Crédit pour retraité Prêts aidés Prêt à taux zéro (PTZ) Prêt accession sociale (PAS) Crédit immobilier Prêt relais immobilier Prêt immobilier avant divorce, Condition pour prêt immobilier, Prêt immobilier entreprise Prêt immobilier sans apport Crédit travaux Renégociation de crédit immobilier Prêt conventionné Les prêts spécifiques Prêt entre particuliers Location avec option d'achat Crédit d'impôt Mini crédit - micro crédit. Prêt entre particuliers : une solution simple et rapide. Le crédit entre particuliers se développe aujourd'hui un peu partout dans le monde et particulièrement en France depuis 1981 .</p>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <!-- End Bootstrap -->
+
+    </div>
+    <!-- End Overview -->
+    
+    <!-- Two colom full width 2 -->
+    <div id="two-colom" class="two-colom-section inner-page-grey-section">
+
+        <!-- Bootstrap -->
+        <div class="container">
+            <div class="row">
+            
+            	<!-- left colom -->
+            	<div class="col-md-6 col-sm-12 ">
+
+					<div class="left-colom">
+                        <h2>MENTIONS LÉGALES
+                        </h2>
+                        
+                        <!-- paragraph -->
+						<p>CREDIT-DE-PROXIMITE-POUR-TOUS.COM est un site édité  <a href="Emprunter.php">Cliquez ici pour toutes demandes de crédit</a> par CREDIT IMMOBILIER DIEBOLDEL, RCS 438 058 795,
+CREDIT IMMOBILIER DIEBOLDEL est une société spécialisée dans le secteur d'activité de la distribution de crédit.</p>
+    					
+                        <!-- list -->
+                        <ul>
+                        	<li>Editeur du site : CREDIT IMMOBILIER DIEBOLDEL </li>
+                            <li>Immatriculée au RCS 438 058 795</li>
+                            <li>SIRET : 43805879844019 </li>
+                            <li>Siège : Non loin Consolato Generale di Romania a Trieste</li>
+                            <li>Téléphone :  +33 755 403 488 & +40 771 132 810  </li>
+                            <li>Email : contact@credit-de-proximite-pour-tous.com </li>
+                        </ul>
+                        
+                    </div>
+                    
+                </div>
+                
+                <!-- Right colom -->
+                <div class="col-md-6 col-sm-12">
+                	
+                	<!-- image -->
+                    <div class="customsection-2">
+						<img src="images/groupe16.jpg" alt="image">
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <!-- End Bootstrap -->
+
+    </div>
+    <!-- End Two colom full width 2 -->
+    
+    
+    
+   
+   <?php include('footer.php')?>
